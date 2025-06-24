4d171474ef582ffe22011f0cabb002717b52bd74
man-navlakha <dhruvsharma56780@gmail.com>
2025-06-24 15:19:56 +0530

    Adding logs

--- chat_bot/views.py
+++ chat_bot/views.py
@@ -11,18 +11,18 @@ collection_name = "pdf_documents"
 # ===================== Django APIView =====================
 @method_decorator(csrf_exempt, name='dispatch')
 class pdf(APIView):
-     def post(self, request):
-        # Referal = request.data.get("Referal")
-        QuePdf = request.data.get("QuePdf")
+    def post(self, request):
+        Referal = request.FILES.get("Referal")
+        QuePdf = request.FILES.get("QuePdf")
         input_grapg = {
-            # "Referal": io.BytesIO(Referal),
-            "QuePdf": QuePdf.read() if QuePdf else None,
+            # "Referal": io.BytesIO(Referal.read()) if Referal else None,
+            "QuePdf": io.BytesIO(QuePdf.read()) if QuePdf else None,
             "collection_name": collection_name,
             "Ans": [],
             "messages": [],
         }
         try:
-            output =main_graph.invoke(input_grapg)
+            output = main_graph.invoke(input_grapg)
             pdf_id = output.get("pdf_id")
             return Response({"message": "Process started", "status": "success", "pdf_id": pdf_id}, status=200)
         except Exception as e:
